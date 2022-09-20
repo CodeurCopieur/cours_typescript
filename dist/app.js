33d370463d6d8872418271db08e563509f4807a0
Widdy LOUIS <louisws@outlook.fr>
2022-09-20 23:33:19 +0200

    come back

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,14 @@
-console.log('hello :)');
-const compteur = document.querySelector('#compteur');
-let i = 0;
-const incre = (e) => {
-    i++;
-    compteur.querySelector('span').innerText = i.toString();
-};
-compteur.addEventListener('click', incre);
+"use strict";
+// console.log('hello :)');
+// const compteur = document.querySelector('#compteur');
+// let i = 0;
+// const incre = (e) => {
+//   i++;
+//   compteur.querySelector('span').innerText = i.toString() 
+// }
+// compteur.addEventListener('click', incre)
+const a = "typescript";
+const b = 3;
+const c = true;
+const d = null;
+const arr = ['a', 'b', 'c'];
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-console.log('hello :)');
+// console.log('hello :)');
 
-const compteur = document.querySelector('#compteur');
+// const compteur = document.querySelector('#compteur');
 
-let i = 0;
+// let i = 0;
 
-const incre = (e) => {
-  i++;
-  compteur.querySelector('span').innerText = i.toString() 
-}
+// const incre = (e) => {
+//   i++;
+//   compteur.querySelector('span').innerText = i.toString() 
+// }
 
-compteur.addEventListener('click', incre)
+// compteur.addEventListener('click', incre)
+
+const a : string = "typescript";
+const b : number = 3;
+const c : boolean = true;
+const d : null = null;
+const arr : string[] = ['a', 'b', 'c'];
