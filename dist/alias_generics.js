682f23a7d6dc35a3ac9299c1e9f1e4c76c0af368
Widdy LOUIS <louisws@outlook.fr>
2022-09-24 23:19:38 +0200

    Alias

---
+++ dist/alias_generics.js
@@ -0,0 +1,5 @@
+"use strict";
+// Alias 
+const user = { firstname: "dev", lastname: "front" };
+const dateString = 'DateString';
+// Generics
---
+++ src/alias_generics.ts
@@ -0,0 +1,10 @@
+// Alias 
+
+type User = {firstname: string, lastname: string}
+type DateString = string;
+type Id = string | number;
+
+const user: User = {firstname: "dev", lastname: "front"}
+const dateString: DateString = 'DateString'
+
+// Generics
