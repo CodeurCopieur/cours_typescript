5edc8778bf0559eceef421139815b74020b0cec0
Widdy LOUIS <louisws@outlook.fr>
2022-09-25 20:19:37 +0200

    Alias & Generics

--- dist/alias_generics.js
+++ dist/alias_generics.js
@@ -3,3 +3,13 @@
 const user = { firstname: "dev", lastname: "front" };
 const dateString = 'DateString';
 // Generics
+function identity(arg) {
+    return arg;
+}
+const a = identity("3");
+// type Id
+function first(arg) {
+    return arg[0];
+}
+const b = first(["a", "b", "c", 4]);
+const c = document.querySelector('#compteur');
--- src/alias_generics.ts
+++ src/alias_generics.ts
@@ -1,6 +1,6 @@
 // Alias 
 
-type User = {firstname: string, lastname: string}
+type User = {firstname: string | number, lastname: string}
 type DateString = string;
 type Id = string | number;
 
@@ -8,3 +8,30 @@ const user: User = {firstname: "dev", lastname: "front"}
 const dateString: DateString = 'DateString'
 
 // Generics
+
+type Firstname  = User['firstname']; // ou keyof User
+
+function identity<ArgType>(arg: ArgType): ArgType {
+  return arg;
+}
+
+const a = identity<string>("3")
+const firstN = identity<Firstname>("test")
+
+// type Id
+function first<Id>(arg: Id[]): Id {
+  return arg[0];
+}
+
+const b = first(["a", "b", "c", 4]);
+
+
+const c = document.querySelector<HTMLButtonElement>('#compteur');
+
+
+function consoleSize<Type extends {length :number}>(arg:Type): Type {
+  console.log(arg.length);
+  return arg;
+}
+
+const d = consoleSize([3, 5, 7])
